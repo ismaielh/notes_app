231706cad2f9c528a4c6d440a8c6b47b68730553
ismaiel <asmaiel649@gmail.com>
2025-02-16 00:25:56 +0300

    display data part tow

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
-  const NoteItem({super.key});
-
+  const NoteItem({super.key, required this.note});
+  final NoteModel note;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -18,7 +19,7 @@ class NoteItem extends StatelessWidget {
       child: Container(
         padding: EdgeInsets.only(top: 24, bottom: 24, left: 16),
         decoration: BoxDecoration(
-          color: Color.fromARGB(255, 251, 178, 69),
+          color: Color(note.color),
           borderRadius: BorderRadius.circular(16),
         ),
         child: Column(
@@ -26,12 +27,12 @@ class NoteItem extends StatelessWidget {
           children: [
             ListTile(
               title: Text(
-                "Flutter Tips",
+                note.title,
                 style: TextStyle(color: Colors.black, fontSize: 26),
               ),
               subtitle: Padding(
                 padding: const EdgeInsets.symmetric(vertical: 16),
-                child: Text("Build your career whit tharwat samy",
+                child: Text(note.subtitle,
                     style: TextStyle(
                         color: Colors.black.withValues(alpha: 0.5),
                         fontSize: 18)),
@@ -46,7 +47,7 @@ class NoteItem extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.only(right: 24),
-              child: Text("May31,2025",
+              child: Text(note.date.,
                   style: TextStyle(
                       color: Colors.black.withValues(alpha: 0.5),
                       fontSize: 16)),
